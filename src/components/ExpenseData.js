37360ca17e81844e1b63edcf5dc141364e562b0a
aditi <aditiparihar179@gmail.com>
2025-07-23 12:20:34 +0530

    add functionality of adding data in table by form

---
+++ src/components/ExpenseData.js
@@ -0,0 +1,24 @@
+export default [{
+    id:crypto.randomUUID(),
+    title:'milk',
+    category:"grocery",
+    amount:40,
+},
+{
+    id:crypto.randomUUID(),
+    title:'Shirt',
+    category:"clothes",
+    amount:600,
+},
+{
+    id:crypto.randomUUID(),
+    title:'vegetables',
+    category:"grocery",
+    amount:100,
+},
+{
+    id:crypto.randomUUID(),
+    title:'Electricity Bill',
+    category:"bill",
+    amount:1100,
+},]
